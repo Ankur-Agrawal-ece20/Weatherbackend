1ee95fc1c4dd02e5b246a023b361601756f11aa0
Ankur-Agrawal-ece20 <ankur.agrawal.ece20@itbhu.ac.in>
2021-10-24 00:34:34 +0530

    Updated settings

--- WeatherBackend/settings.py
+++ WeatherBackend/settings.py
@@ -28,6 +28,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','weather--backend.herokuapp.com']
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Application definition
 
