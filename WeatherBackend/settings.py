40a3c58697c033b365dbbf13164b7761eaae3539
Ankur-Agrawal-ece20 <ankur.agrawal.ece20@itbhu.ac.in>
2021-10-24 00:18:24 +0530

    Added domain name

--- WeatherBackend/settings.py
+++ WeatherBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jytom@m_a2r5w0#s1vzs1)xb_$_9=5+ymrk9c*j_79hprikmyj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','weather--backend.herokuapp.com']
 
 
 # Application definition
